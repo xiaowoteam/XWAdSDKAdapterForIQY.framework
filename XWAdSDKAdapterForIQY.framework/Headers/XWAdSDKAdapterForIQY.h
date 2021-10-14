b2686fffe5311243c8d06857d9e2170c81d218bf
laole918 <laole918@qq.com>
2021-10-14 18:31:53 +0800

    first commit

---
+++ XWAdSDKAdapterForIQY.framework/Headers/XWAdSDKAdapterForIQY.h
@@ -0,0 +1,18 @@
+//
+//  XWAdSDKAdapterForIQY.h
+//  XWAdSDKAdapterForIQY
+//
+//  Created by laole918 on 2021/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAdSDKAdapterForIQY.
+FOUNDATION_EXPORT double XWAdSDKAdapterForIQYVersionNumber;
+
+//! Project version string for XWAdSDKAdapterForIQY.
+FOUNDATION_EXPORT const unsigned char XWAdSDKAdapterForIQYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAdSDKAdapterForIQY/PublicHeader.h>
+
+
